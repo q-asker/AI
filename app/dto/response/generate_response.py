4758adfa0388794fb46521d255924c7c7b0c590a
Oh YoungJe <pine7420@naver.com>
2025-05-28 15:04:58 +0900

    [ICC-39] 구현 완료

--- app/dto/response/generate_response.py
+++ app/dto/response/generate_response.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
+from pydantic import Field
 
 from app.dto.model.problem import Problem
 
 
 class GenerateResponse(BaseModel):
-    title: str
+    title: str = Field(
+        default="",
+        description="문제집의 제목입니다.",
+    )
     problems: list[Problem]
--- app/router/generate_router.py
+++ app/router/generate_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from app.dto.request.generate_request import GenerateRequest
+from app.dto.response.generate_response import GenerateResponse
 from app.service.generate_service import GenerateService
 
 router = APIRouter()
@@ -13,5 +14,5 @@ def get_generate_service():
 @router.post("/generate")
 async def generate(
     request: GenerateRequest, generate_service=Depends(get_generate_service)
-):
+) -> GenerateResponse:
     return await generate_service.generate(request)
