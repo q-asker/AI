eb9d507bd160a2edbce5bcafb6bda0585ff97386
Oh YoungJe <pine7420@naver.com>
2025-06-04 22:29:29 +0900

    [ICC-67] 응답 구조 수정 완료

--- app/dto/response/generate_response.py
+++ app/dto/response/generate_response.py
@@ -1,16 +1,17 @@
-from pydantic import BaseModel, Field
 from typing import List
 
+from pydantic import BaseModel
 
-class Selection(BaseModel):
-    content: str = Field(description="선택지 내용입니다.")
-    correct: bool = Field(description="정답 여부입니다. 정답이면 True, 오답이면 False입니다.")
+from app.dto.model.problem_set import Selection
 
-class Problem(BaseModel):
-    number: int = Field(description="문제 번호입니다.")
-    title: str = Field(description="문제 내용입니다.")
-    selections: List[Selection] = Field(description="선택지 목록입니다.")
-    explanation: str = Field(description="문제에 대한 해설입니다.")
+
+class ProblemResponse(BaseModel):
+    number: int
+    title: str
+    selections: List[Selection]
+    explanation: str
+    referencedPages: List[int]
+
 
 class GenerateResponse(BaseModel):
-    quiz: List[Problem] = Field(description="퀴즈 목록입니다.")
+    quiz: List[ProblemResponse]
