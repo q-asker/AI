507afc2ab3458ff5fe9205bd3437b1db4313de55
Adansonia <lcw061145@gmail.com>
2025-06-05 20:19:07 +0900

    [ICC-63] Webb's DOK
    # Conflicts:
    #	app/service/generate_service.py

--- app/dto/request/generate_request.py
+++ app/dto/request/generate_request.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel
+from enum import Enum
 
+
+class DOKLevel(str, Enum):
+    RECALL = "RECALL"
+    SKILLS = "SKILLS"
+    STRATEGIC = "STRATEGIC"
 
 class GenerateRequest(BaseModel):
     uploadedUrl: str
     quizCount: int
+    difficultyType: DOKLevel
     type: str
