9ff2ad41bede06f2e7335b7dc76d075b94f1b2df
Oh YoungJe <pine7420@naver.com>
2025-10-22 17:25:41 +0900

    [ICC-167] 리팩터링

--- app/adapter/request_generate_specific_explanation_to_bedrock.py
+++ app/adapter/request_generate_specific_explanation_to_bedrock.py
@@ -5,19 +5,15 @@ from typing import List, Tuple, Optional
 from uuid import uuid4
 
 import boto3
-import httpx
 from dotenv import load_dotenv
-from langchain_core.output_parsers import JsonOutputParser
 
+from app.adapter.process_on_local import process_on_local
 from app.dto.model.generated_result import GeneratedResult
-from app.dto.model.problem_set import ProblemSet
 from app.util.logger import logger
 from app.util.redis_util import RedisUtil
 
 load_dotenv()
 aws_region = os.getenv("AWS_REGION")
-aws_lambda_url = os.getenv("AWS_LAMBDA_URL")
-aws_sqs_url = os.getenv("AWS_SQS_URL")
 aws_mcp_sqs_url = os.getenv("AWS_MCP_SQS_URL")
 time_out = int(os.getenv("TIME_OUT"))
 
@@ -25,7 +21,7 @@ sqs = boto3.client("sqs", region_name=aws_region)
 redis_util = RedisUtil()
 
 
-async def request_to_bedrock(bedrock_contents, mcp_mode=False):
+async def request_specific_explanation_to_bedrock(bedrock_contents):
     keys = []
     quiz_count = len(bedrock_contents)
 
@@ -41,10 +37,10 @@ async def request_to_bedrock(bedrock_contents, mcp_mode=False):
     await asyncio.gather(*tasks, return_exceptions=True)  # Redis에 요청 내용 저장
 
     if os.getenv("ENV") == "local":
-        await process_on_local(keys, mcp_mode)
+        await process_on_local(keys, True)
     elif os.getenv("ENV") == "remote":
         loop = asyncio.get_running_loop()
-        await loop.run_in_executor(None, process_on_remote, keys, mcp_mode)  # SQS 사용
+        await loop.run_in_executor(None, publish_to_sqs, keys)  # SQS 사용
     else:
         raise ValueError("ENV must be either 'local' or 'remote'")
 
@@ -67,7 +63,7 @@ async def collect_quizzes(baseKey, quiz_count) -> List[GeneratedResult]:
                 status, result = try_make_generated_result(msg, seen_sequences)
                 if status == "ok":
                     quizzes.append(result)
-                if published_count == quiz_count:
+                if published_count == quiz_count + 1:
                     break
 
     except asyncio.TimeoutError:
@@ -101,12 +97,6 @@ def try_make_generated_result(
 
         generated_result = GeneratedResult(**response_json)
 
-        parser = JsonOutputParser(pydantic_object=ProblemSet)
-        temp = parser.parse(generated_result.generated_text)
-
-        if 4 < len(temp.get("quiz")[0].get("selections")):
-            return "fail", None
-
         seen_sequences.add(sequence)
         return "ok", generated_result
     except Exception as e:
@@ -114,45 +104,20 @@ def try_make_generated_result(
         return "fail", None
 
 
-async def process_on_local(keys, mcp_mode):
-    payload = {"keys": keys, "mcp_mode": mcp_mode}
-    async with httpx.AsyncClient() as client:
-        response = await client.post(aws_lambda_url, json=payload)
-        try:
-            response.raise_for_status()
-        except httpx.HTTPError as exc:
-            logger.error(f"Lambda 호출 실패: {exc}")
-            raise exc
-
-
-def process_on_remote(keys, mcp_mode):
+def publish_to_sqs(keys):
     message_group_id = keys[0].split(":")[1]
     for i in range(0, len(keys), 10):
-        if mcp_mode:
-            entries = []
-            batch = keys[i: i + 10]
-            for j, key in enumerate(batch):
-                entries.append(
-                    {
-                        "Id": str(j),
-                        "MessageBody": key,
-                        "MessageGroupId": message_group_id,
-                    }
-                )
-            response = sqs.send_message_batch(QueueUrl=aws_mcp_sqs_url, Entries=entries)
-
-        else:
-            entries = []
-            batch = keys[i: i + 10]
-            for j, key in enumerate(batch):
-                entries.append(
-                    {
-                        "Id": str(j),
-                        "MessageBody": key,
-                    }
-                )
-            response = sqs.send_message_batch(QueueUrl=aws_sqs_url, Entries=entries)
-
+        batch = keys[i: i + 10]
+        entries = []
+        for j, key in enumerate(batch):
+            entries.append(
+                {
+                    "Id": str(j),
+                    "MessageBody": key,
+                    "MessageGroupId": message_group_id,
+                }
+            )
+        response = sqs.send_message_batch(QueueUrl=aws_mcp_sqs_url, Entries=entries)
         if response.get("Failed"):
             raise Exception("Failed to send messages to SQS")
         else:
