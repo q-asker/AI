d6f2a9020769c5aa536c24fc80001f3e98c397a6
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-01-22 00:08:21 +0900

    [ICC-233] 구현 완료

--- app/util/parsing.py
+++ app/util/parsing.py
@@ -1,12 +1,9 @@
-import os
-import tempfile
+from http.client import HTTPException
 from typing import List
 
-import fitz  # PyMuPDF
+import fitz
 import requests
-from pptx import Presentation
-
-max_page_count = 100
+from fastapi import HTTPException
 
 
 def process_file(uploaded_url: str, page_numbers: List[int]) -> List[str]:
@@ -17,48 +14,24 @@ def process_file(uploaded_url: str, page_numbers: List[int]) -> List[str]:
         if uploaded_url.endswith(".pdf"):
             pdf_documents = fitz.open(stream=file_content, filetype="pdf")
 
-            if max_page_count < len(pdf_documents):
-                raise ValueError(f"페이지 수가 {max_page_count}페이지를 초과합니다.")
-
+            content_length = 0
             one_based_pages = [""]
             for pdf_document in pdf_documents:
+                content_length += len(pdf_document.get_text())
                 one_based_pages.append(pdf_document.get_text())
             pdf_documents.close()
 
-            if not page_numbers:
-                return one_based_pages
-
+            if content_length < 10:
+                raise HTTPException(
+                    status_code=400,
+                    detail="파일에 텍스트가 충분하지 않습니다. \n 파일을 연 뒤, 텍스트가 선택되는지 확인해주세요. \n 선택되지 않는다면 OCR 변환이 필요합니다.",
+                )
             select_pages = [
                 one_based_pages[i] for i in page_numbers if 0 < i < len(one_based_pages)
             ]
+
             return [""] + select_pages
 
-        elif uploaded_url.endswith(".pptx"):
-            with tempfile.NamedTemporaryFile(suffix=".pptx", delete=False) as temp_file:
-                temp_file.write(file_content)
-                temp_file_path = temp_file.name
-
-            presentation = Presentation(temp_file_path)
-            if max_page_count < len(presentation.slides):
-                raise ValueError(f"페이지 수가 {max_page_count}페이지를 초과합니다.")
-
-            one_based_pages = [""]
-            for slide in presentation.slides:
-                slide_text = ""
-                for shape in slide.shapes:
-                    if hasattr(shape, "text"):
-                        slide_text += shape.text + "\n\n"
-                one_based_pages.append(slide_text)
-
-            os.unlink(temp_file_path)
-
-            if not page_numbers:
-                return one_based_pages
-
-            select_pages = [
-                one_based_pages[i] for i in page_numbers if 0 < i < len(one_based_pages)
-            ]
-            return [""] + select_pages
         else:
             raise ValueError("지원하지 않는 파일 형식입니다.")
     except Exception as e:
